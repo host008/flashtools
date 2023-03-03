e4d8e7080551bc4d53bf8a5ca4d6ecf83e67d2a1
Justin "Qwerty" Sun <89545577+host008@users.noreply.github.com>
2023-03-02 16:47:05 -0800

    sig

--- flashtools.py
+++ flashtools.py
@@ -1,8 +1,5 @@
 '''
-
 made by @host008
-
-
 
 NOTE: i suck at programming
 '''
@@ -14,6 +11,7 @@ html = '''
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- code sucks i know -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
